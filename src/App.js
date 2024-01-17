ab0d8123847a1022013271e2a2d2c5e2df7141dd
taegen-seto <taegenseto@gmail.com>
2024-01-17 16:06:45 +1000

    moved footer from index.html to App.js file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ export default function App() {
   return (
     <div classNameName="App">
       <Weather />
+
+      <footer>
+        <a
+          href="https://github.com/taegen-seto"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Coded by Taegen Seto
+        </a>
+        🖤
+        <a
+          href="https://github.com/taegen-seto/aplicacion-del-tiempo"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        🐈‍⬛
+        <a
+          href="https://rad-ganache.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
