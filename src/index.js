947806bf10bba0989bbc8fe9183cccbaf653b48a
taegen-seto <taegenseto@gmail.com>
2024-01-17 16:03:39 +1000

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+
 import "./App.css";
 import "./Weather.css";
 import axios from "axios";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
