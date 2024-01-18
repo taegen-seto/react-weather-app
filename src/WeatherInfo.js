2651b4a0905ec65ad337fe8c924351e018dde9c1
taegen-seto <taegenseto@gmail.com>
2024-01-18 14:49:21 +1000

    tidy classNames

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,27 +6,27 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <div className="weather-app-data">
+      <div className="data">
         <div>
-          <h1 className="weather-app-city">{props.data.city}</h1>
-          <p className="weather-app-details">
-            <span className="weather-app-date">
+          <h1 className="city">{props.data.city}</h1>
+          <p className="details">
+            <span className="date">
               <FormattedDate date={props.data.date} />
             </span>
             <br />
-            <span className="weather-description"></span>
+            <span className="description"></span>
             {props.data.description}
             <br />
             humidity: <strong> {props.data.humidity}% </strong>
             wind: <strong> {props.data.wind}km/hr </strong>
           </p>
         </div>
-        <div className="weather-app-temperature-container">
+        <div className="temperature-container">
           <WeatherIcon code={props.data.icon} />
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
       </div>
-      <div className="weather-forecast"></div>
+      <div className="forecast"></div>
     </div>
   );
 }
